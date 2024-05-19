e2fc5fd0a0650bf8f9022ad2832d807094df59c3
Aakashdeep Sil <aakashdeep.work@gmail.com>
2024-05-19 12:01:38 -0500

    added carousel to home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,34 +1,231 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+import 'package:infinite_carousel/infinite_carousel.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 import 'constants.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // Wheater to loop through elements
+  final bool _loop = true;
+
+  // Scroll controller for carousel
+  late InfiniteScrollController _controller;
+
+  // Maintain current index of carousel
+  int _selectedIndex = 0;
+
+  // Width of each item
+  double? _mainCarouselItemWidth;
+  double? _topWebToonsCarouselItemWidth;
+
+  // Get screen width of viewport.
+  double get screenWidth => MediaQuery.of(context).size.width;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = InfiniteScrollController(initialItem: _selectedIndex);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _mainCarouselItemWidth = screenWidth - 50;
+    _topWebToonsCarouselItemWidth = screenWidth - 300;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  List<String> kDemoImages = [
+    'https://i.pinimg.com/originals/7f/91/a1/7f91a18bcfbc35570c82063da8575be8.jpg',
+    'https://www.absolutearts.com/portfolio3/a/afifaridasiddique/Still_Life-1545967888l.jpg',
+    'https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/53415/72138/1597120261997_IMG_20200811_095922__49127.1597493165.jpg?c=2',
+    'https://i.pinimg.com/originals/47/7e/15/477e155db1f8f981c4abb6b2f0092836.jpg',
+    'https://images.saatchiart.com/saatchi/770124/art/3760260/2830144-QFPTZRUH-7.jpg',
+    'https://images.unsplash.com/photo-1471943311424-646960669fbc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c3RpbGwlMjBsaWZlfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80',
+    'https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/40895/55777/1526876829723_P211_24X36__2018_Stilllife_15000_20090__91926.1563511650.jpg?c=2',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIUsxpakPiqVF4W_rOlq6eoLYboOFoxw45qw&usqp=CAU',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar('Home'),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You are home',
-              style: TextStyle(fontSize: 42),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Supabase.instance.client.auth.signOut();
-                Navigator.of(context).pushReplacementNamed('/');
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 200,
+            child: InfiniteCarousel.builder(
+              itemCount: kDemoImages.length,
+              itemExtent: _mainCarouselItemWidth ?? 50,
+              scrollBehavior: kIsWeb
+                  ? ScrollConfiguration.of(context).copyWith(
+                      dragDevices: {
+                        // Allows to swipe in web browsers
+                        PointerDeviceKind.touch,
+                        PointerDeviceKind.mouse,
+                      },
+                    )
+                  : null,
+              loop: _loop,
+              controller: _controller,
+              onIndexChanged: (index) {
+                if (_selectedIndex != index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                }
               },
-              child: const Text(
-                'Log Out',
-                style: TextStyle(fontWeight: FontWeight.bold),
+              itemBuilder: (context, itemIndex, realIndex) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      _controller.animateToItem(realIndex);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: kElevationToShadow[2],
+                        image: DecorationImage(
+                          image: NetworkImage(kDemoImages[itemIndex]),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Top WebToons',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          SizedBox(
+            height: 200,
+            child: InfiniteCarousel.builder(
+              itemCount: kDemoImages.length,
+              itemExtent: _topWebToonsCarouselItemWidth ?? 50,
+              scrollBehavior: kIsWeb
+                  ? ScrollConfiguration.of(context).copyWith(
+                      dragDevices: {
+                        // Allows to swipe in web browsers
+                        PointerDeviceKind.touch,
+                        PointerDeviceKind.mouse,
+                      },
+                    )
+                  : null,
+              loop: _loop,
+              controller: _controller,
+              onIndexChanged: (index) {
+                if (_selectedIndex != index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                }
+              },
+              itemBuilder: (context, itemIndex, realIndex) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      _controller.animateToItem(realIndex);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Column(children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              image: DecorationImage(
+                                image: NetworkImage(kDemoImages[itemIndex]),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          'WebToon Title',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ]),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Supabase.instance.client.auth.signOut();
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+            child: const Text(
+              'Log Out',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
               ),
-            )
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          topRight: Radius.circular(15.0),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.teal,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              tooltip: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Explore',
+              tooltip: 'Explore',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.library_books),
+              label: 'Favorites',
+              tooltip: 'Favorites',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+              tooltip: 'Profile',
+            ),
           ],
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
         ),
       ),
     );
