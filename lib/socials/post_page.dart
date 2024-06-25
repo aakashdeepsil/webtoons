f33db274c26ed5e21ea74717e04e35999274a1f5
Aakashdeep Sil <aakashdeep.work@gmail.com>
2024-06-24 22:11:59 -0500

    post page added

---
+++ lib/socials/post_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:webtoons/socials/post.dart';
+
+class PostPage extends StatefulWidget {
+  final String? postId;
+
+  const PostPage({super.key, required this.postId});
+
+  @override
+  State<PostPage> createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Post Page'),
+      ),
+      body: Post(),
+    );
+  }
+}
